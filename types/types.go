7dbf8ec5417a2e717062c6a371dae6eb8383c93a
emomovg <golya-1996@mail.ru>
2024-08-20 09:24:16 +0500

    module types

---
+++ types/types.go
@@ -0,0 +1,61 @@
+package types
+
+type Money int64
+
+type Duration int64
+
+const (
+	Nanosecond  Duration = 1
+	Microsecond          = 1000 * Nanosecond
+	Millisecond          = 1000 * Microsecond
+	Minute               = 60 * Nanosecond
+	Minutes              = 60 * Microsecond
+	Hour                 = 60 * Minute
+)
+
+const (
+	TJS = "TJS"
+	RUB = "RUB"
+	USD = "USD"
+	EUR = "EUR"
+)
+
+type Card struct {
+	Id       int
+	Pan      string
+	Balance  Money
+	Currency Currency
+	Color    string
+	Name     string
+	Active   bool
+}
+
+type Currency string
+
+type PAN string
+
+type Payment struct {
+	Id        int
+	AccountId int64
+	Amount    Money
+	Category  Category
+	Status    PaymentStatus
+}
+
+type PaymentSource struct {
+	Type    string
+	Number  string
+	Balance Money
+}
+
+type Category string
+
+type PaymentStatus string
+
+type Account struct {
+	Id      int64
+	phone   Phone
+	Balance Money
+}
+
+type Phone string
